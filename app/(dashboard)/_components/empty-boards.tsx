53a5e8a7df184751120ab6ef3ecee8045e0eb56a
ktjn1129 <97653478+ktjn1129@users.noreply.github.com>
2024-03-30 16:38:54 +0900

    add create mutation

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -1,10 +1,39 @@
+"use client";
+
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { api } from "@/convex/_generated/api";
+import { Button } from "@/components/ui/button";
+import { useOrganization } from "@clerk/nextjs";
+import { toast } from "sonner";
+
 export const EmptyBoards = () => {
+  const { organization } = useOrganization();
+  const { mutate, pending } = useApiMutation(api.board.create);
+
+  const onClick = () => {
+    if (!organization) return;
+
+    mutate({
+      orgId: organization.id,
+      title: "Untitled",
+    })
+      .then((id) => {
+        toast.success("Board created");
+      })
+      .catch(() => toast.error("Failed to create board"));
+  };
+
   return (
     <div className="h-full flex flex-col items-center justify-center">
       <h2 className="text-2xl font-semibold">Create your first board!</h2>
       <p className="test-muted-foreground text-sm mt-2">
         Start by creating a board for your organization
       </p>
+      <div className="mt-6">
+        <Button disabled={pending} onClick={onClick} size="lg">
+          Create board
+        </Button>
+      </div>
     </div>
   );
 };
